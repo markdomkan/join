f880d05b00def0368e428a718c5b3d446e420621
Marc Domenech Canalda <markdomkan@hotmail.com>
2024-08-06 13:39:38 +0200

    adding global css and routing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+
+<svelte:head>
+	<meta name="theme-color" media="(prefers-color-scheme: light)" content="cyan" />
+	<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1a1a1a" />
+</svelte:head>
+
+<main>
+
+	
+	<slot />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+	}
+</style>
